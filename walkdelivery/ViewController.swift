7a6ee7d6702b2d8d9d15cb3bef6508cf8be55080
AndreyLebedev <ASLebedev.SBT@sberbank.ru>
2017-05-18 14:57:44 +0300

    initial commit

---
+++ walkdelivery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  walkdelivery
+//
+//  Created by AndreyLebedev on 16/05/2017.
+//  Copyright © 2017 lebedac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
