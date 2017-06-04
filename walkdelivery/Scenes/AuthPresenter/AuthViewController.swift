9284a672a8385f1fad34a4daaabdd2e525f2e748
AndreyLebedev <ASLebedev.SBT@sberbank.ru>
2017-06-04 11:01:53 +0300

    authloinview implemented

--- walkdelivery/Scenes/AuthPresenter/AuthViewController.swift
+++ walkdelivery/Scenes/AuthPresenter/AuthViewController.swift
@@ -13,48 +13,9 @@ class AuthViewController: UIViewController {
 	
 	var output: AuthViewOutput?
 	
-	// MARK: Views
-	var loginView = UIView(frame: CGRect.zero)
-	var textFieldEmail: UITextField = {
-		let textField = UITextField(frame: CGRect.zero)
-		textField.translatesAutoresizingMaskIntoConstraints = false
-		textField.borderStyle = UITextBorderStyle.roundedRect
-		textField.backgroundColor = UIColor.white
-		textField.placeholder = "Email"
-		return textField
-	}()
-	var textFieldPassword: UITextField = {
-		let textField = UITextField(frame: CGRect.zero)
-		textField.translatesAutoresizingMaskIntoConstraints = false
-		textField.borderStyle = UITextBorderStyle.roundedRect
-		textField.backgroundColor = UIColor.white
-		textField.placeholder = "Password"
-		return textField
-	}()
-	var loginButton: UIButton = {
-		let button = UIButton(type: UIButtonType.custom)
-		button.translatesAutoresizingMaskIntoConstraints = false
-		button.setTitle("Login", for: .normal)
-		button.titleLabel?.textAlignment = NSTextAlignment.center
-		button.titleLabel?.textColor = UIColor.white
-		button.backgroundColor = UIColor.orange
-		return button
-	}()
-	var signupButton: UIButton = {
-		let button = UIButton(type: UIButtonType.custom)
-		button.translatesAutoresizingMaskIntoConstraints = false
-		button.setTitle("Sign up", for: .normal)
-		button.titleLabel?.textAlignment = NSTextAlignment.center
-		button.titleLabel?.textColor = UIColor.white
-		button.backgroundColor = UIColor.clear
-		return button
-	}()
+	var loginView = LoginView(frame: CGRect.zero)
 	
-	// MARK: Constants
 	let LoginViewLeft: CGFloat = 30.0
-	let LoginViewVertOffset: CGFloat = 7.0
-	let LoginButtonTop: CGFloat = 15.0
-	let SignupButtonTop: CGFloat = 10.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -66,63 +27,18 @@ extension AuthViewController: AuthViewInput {
 	
 	func setupViews() {
 		view.translatesAutoresizingMaskIntoConstraints = false
-		view.backgroundColor = UIColor.green
+		view.backgroundColor = UIColor.lightGray
 		setupLoginView()
 	}
 	
 	func setupLoginView() {
 		self.view.addSubview(loginView)
-		loginView.backgroundColor = UIColor.red
-		setupEmailView()
-		setupPasswordView()
-		setupLoginButton()
-		setupSignupButton()
 		
-		self.view.layoutIfNeeded()
-		
-		let loginViewHeight = textFieldEmail.frame.height + LoginViewVertOffset + textFieldPassword.frame.height +
-						      LoginButtonTop + loginButton.frame.height + SignupButtonTop + signupButton.frame.height
 		loginView.snp.makeConstraints { make in
-			make.height.equalTo(loginViewHeight)
 			make.center.equalTo(self.view)
 			make.leftMargin.equalTo(self.view).offset(LoginViewLeft)
 			make.rightMargin.equalTo(self.view).offset(LoginViewLeft)
 		}
-	}
-	
-	func setupEmailView() {
-		loginView.addSubview(textFieldEmail)
-		textFieldEmail.snp.makeConstraints { make in
-			make.top.equalTo(loginView.snp.top)
-			make.left.equalTo(loginView)
-			make.right.equalTo(loginView)
-		}
-	}
-	
-	func setupPasswordView() {
-		loginView.addSubview(textFieldPassword)
-		textFieldPassword.snp.makeConstraints { make in
-			make.top.equalTo(textFieldEmail.snp.bottom).offset(LoginViewVertOffset)
-			make.left.equalTo(loginView)
-			make.right.equalTo(loginView)
-		}
-	}
-	
-	func setupLoginButton() {
-		loginView.addSubview(loginButton)
-		loginButton.snp.makeConstraints { make in
-			make.top.equalTo(textFieldPassword.snp.bottom).offset(LoginButtonTop)
-			make.left.equalTo(loginView)
-			make.right.equalTo(loginView)
-		}
-	}
-	
-	func setupSignupButton() {
-		loginView.addSubview(signupButton)
-		signupButton.snp.makeConstraints { make in
-			make.top.equalTo(loginButton.snp.bottom).offset(SignupButtonTop)
-			make.left.equalTo(loginView)
-			make.right.equalTo(loginView)
-		}
+		loginView.updateHeight()
 	}
 }
---
+++ walkdelivery/Scenes/AuthPresenter/Views/LoginView.swift
@@ -0,0 +1,132 @@
+//
+//  LoginView.swift
+//  walkdelivery
+//
+//  Created by AndreyLebedev on 03/06/2017.
+//  Copyright © 2017 lebedac. All rights reserved.
+//
+
+import UIKit
+
+class LoginView: UIView {
+	
+	var textFieldEmail: UITextField = {
+		let textField = UITextField(frame: CGRect.zero)
+		textField.translatesAutoresizingMaskIntoConstraints = false
+		textField.borderStyle = UITextBorderStyle.roundedRect
+		textField.backgroundColor = UIColor.white
+		textField.placeholder = "Email"
+		return textField
+	}()
+	var textFieldPassword: UITextField = {
+		let textField = UITextField(frame: CGRect.zero)
+		textField.translatesAutoresizingMaskIntoConstraints = false
+		textField.borderStyle = UITextBorderStyle.roundedRect
+		textField.backgroundColor = UIColor.white
+		textField.placeholder = "Password"
+		return textField
+	}()
+	var loginButton: UIButton = {
+		let button = UIButton(type: UIButtonType.system)
+		button.translatesAutoresizingMaskIntoConstraints = false
+		button.setTitle("Login", for: .normal)
+		button.titleLabel?.textAlignment = NSTextAlignment.center
+		button.titleLabel?.textColor = UIColor.white
+		button.backgroundColor = UIColor.orange
+		return button
+	}()
+	var signupButton: UIButton = {
+		let button = UIButton(type: UIButtonType.system)
+		button.translatesAutoresizingMaskIntoConstraints = false
+		button.setTitle("Sign up", for: .normal)
+		button.titleLabel?.textAlignment = NSTextAlignment.center
+		button.titleLabel?.textColor = UIColor.white
+		button.backgroundColor = UIColor.clear
+		return button
+	}()
+	
+	private var didSetupConstraints = false
+	
+	// MARK: Constants
+	private let LoginViewVertOffset: CGFloat = 7.0
+	private let LoginButtonTop: CGFloat = 15.0
+	private let SignupButtonTop: CGFloat = 10.0
+
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		setup()
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		setup()
+	}
+	
+	func updateHeight() {
+		self.layoutIfNeeded()
+		let loginViewHeight = textFieldEmail.frame.height + LoginViewVertOffset + textFieldPassword.frame.height +
+			LoginButtonTop + loginButton.frame.height + SignupButtonTop + signupButton.frame.height
+		self.snp.makeConstraints { make in
+			make.height.equalTo(loginViewHeight)
+		}
+	}
+	
+	private func setup() {
+		self.translatesAutoresizingMaskIntoConstraints = false
+		self.backgroundColor = UIColor.clear
+		self.clipsToBounds = true
+		
+		self.addSubview(textFieldEmail)
+		self.addSubview(textFieldPassword)
+		self.addSubview(loginButton)
+		self.addSubview(signupButton)
+		
+		self.setNeedsUpdateConstraints()
+	}
+	
+	override func updateConstraints() {
+		super.updateConstraints()
+		
+		if didSetupConstraints == false {
+			
+			setupEmailViewConstraints()
+			setupPasswordViewConstraints()
+			setupLoginButtonConstrants()
+			setupSignupButtonConstraints()
+			
+			didSetupConstraints = true
+		}
+	}
+	
+	private func setupEmailViewConstraints() {
+		textFieldEmail.snp.makeConstraints { make in
+			make.top.equalTo(self.snp.top)
+			make.left.equalTo(self)
+			make.right.equalTo(self)
+		}
+	}
+	
+	private func setupPasswordViewConstraints() {
+		textFieldPassword.snp.makeConstraints { make in
+			make.top.equalTo(textFieldEmail.snp.bottom).offset(LoginViewVertOffset)
+			make.left.equalTo(self)
+			make.right.equalTo(self)
+		}
+	}
+	
+	private func setupLoginButtonConstrants() {
+		loginButton.snp.makeConstraints { make in
+			make.top.equalTo(textFieldPassword.snp.bottom).offset(LoginButtonTop)
+			make.left.equalTo(self)
+			make.right.equalTo(self)
+		}
+	}
+	
+	private func setupSignupButtonConstraints() {
+		signupButton.snp.makeConstraints { make in
+			make.top.equalTo(loginButton.snp.bottom).offset(SignupButtonTop)
+			make.left.equalTo(self)
+			make.right.equalTo(self)
+		}
+	}
+}
